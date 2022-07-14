db28dbf56dbe43b31cce507542cb43afffe3a658
Hojiakbar-Davurov <hojiakbar0315@gmail.com>
2022-07-14 15:45:14 +0500

    all of run

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,9 +20,7 @@ public class Main {
     );
 
     public static void main(String... args) {
-
         UnluckyVassal unluckyVassal = new UnluckyVassal();
-
         unluckyVassal.printReportForKing(pollResults);
     }
 }
--- src/main/java/UnluckyVassal.java
+++ src/main/java/UnluckyVassal.java
@@ -1,30 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UnluckyVassal {
-    private static List<String> pollResults = List.of(
-            "служанка Аня",
-            "управляющий Семен Семеныч: крестьянин Федя, доярка Нюра",
-            "дворянин Кузькин: управляющий Семен Семеныч, жена Кузькина, экономка Лидия Федоровна",
-            "экономка Лидия Федоровна: дворник Гена, служанка Аня",
-            "доярка Нюра",
-            "кот Василий: человеческая особь Катя",
-            "дворник Гена: посыльный Тошка",
-            "киллер Гена",
-            "зажиточный холоп: крестьянка Таня",
-            "секретарь короля: зажиточный холоп, шпион Т",
-            "шпион Т: кучер Д",
-            "посыльный Тошка: кот Василий",
-            "аристократ Клаус",
-            "просветленный Антон"
-    );
-
+    int countTab = 1;
     List<String> result = new ArrayList<>();
-
 
     public void printReportForKing(List<String> pollResults) {
 
-        //find assistantKing
+        //show king at first
+        System.out.println("король");
+
+        //find and show assistantKing
         int i = 0;
         for (String pollResult : pollResults) {
             String kingAssistant = pollResult.contains(":") ? pollResult.substring(0, pollResult.indexOf(":")) : pollResult;
@@ -37,10 +24,41 @@ public class UnluckyVassal {
             i = 0;
         }
 
-        
-
+        //show inner assistant
+        Collections.sort(result);
         for (String s : result) {
-            System.out.println(s);
+            System.out.println("\t" + s);
+            findAssistant(s, pollResults);
+            countTab--;
+        }
+    }
+
+    /**
+     * this method to show assistant of somebody
+     * @param somebody is assistant's boss
+     * @param listPeople is all of country's people
+     */
+    public void findAssistant(String somebody, List<String> listPeople) {
+        countTab++;
+
+        //insert into array by ','
+        String[] assistants = new String[0];
+        for (String pupil : listPeople) {
+            if (pupil.contains(somebody + ":")) {
+                assistants = pupil.substring(pupil.indexOf(":") + 1).split(",");
+                break;
+            }
+        }
+
+        //show assistant
+        for (String assistant : assistants) {
+            assistant = assistant.trim();
+            for (int i = 0; i < countTab; i++) {
+                System.out.print("\t");
+            }
+            System.out.println(assistant);
+            findAssistant(assistant, listPeople);
+            countTab--;
         }
     }
 }
